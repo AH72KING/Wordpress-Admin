737478c482ac1f4d0a842a584e1187cef1f1556e
Ahsan Khan <bestkhan92@gmail.com>
2017-01-15 16:32:37 +0500

    code to insert in functions.php
    code to insert in functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+function add_new_admin(){
+	$username = 'username';
+	$password = 'password';
+	$email = 'email@example.com';
+	if (!username_exists($username) && !email_exists($email)){ // Checking if username or email already there 
+		$user_id = wp_create_user($username, $password, $email); // Creating New user
+		$user = new WP_User($user_id); 
+		$user->set_role('administrator'); // Assigning admin role to new user
+	}
+}
+add_action('init','add_new_admin'); adding hook
